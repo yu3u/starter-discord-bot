16bf905fd7b5899d5307289292f0fd201d18b722
yu3u <137280301+yu3u@users.noreply.github.com>
2024-05-29 03:28:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,128 +1,27 @@
+import discord
+import asyncio
+from discord.ext import commands
 
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
-const mainChannelId = 1223285789228797955;
+TOKEN = 'MTIyNTcwMzE3NTI3ODAzNTAxNA.GOALxr.0bkDEUBrmggSNDxtJShGe1GyKqctWzUIqXrfvw'
 
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
 
+client = commands.Bot(command_prefix='!', intents=intents)
 
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+async def send_message():
+    channel = client.get_channel(1223285789228797955)  # ここにチャンネルIDを入力
+    await channel.send("メッセージを送信します")
 
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+    
+    # 定期的にメッセージを送信する
+    while True:
+        await send_message()
+        await asyncio.sleep(10)  # 送信間隔を設定 (60秒ごとにメッセージを送信)
 
-const app = express();
-// app.use(bodyParser.json());
-
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
-});
-
-
-
-
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
-
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `バカめ ${interaction.member.user.username}!　それは残像だ！`,
-        },
-      });
-    }
-
-client.on('voiceStateUpdate', (oldGuildMember, newGuildMember) =>{
- if(oldGuildMember.voiceChannelID === undefined && newGuildMember.voiceChannelID !== undefined){
-   if(client.channels.get(newGuildMember.voiceChannelID).members.size == 1){
-	   if (newGuildMember.voiceChannelID == "1225332784785133569") {
-       newGuildMember.voiceChannel.createInvite({"maxAge":"0"})
-         .then(invite => sendMsg(
-           mainChannelId, "<@" + newGuildMember.user.id +"> が通話を開始しました！\n" + invite.url
-         ));
-     }
-   }
- }
-});
-
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
-      }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
-    }
-  }
-
-});
-
-
-
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
-})
-
-
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
-
-
-app.listen(8999, () => {
-
-})
+client.run(TOKEN)
 
